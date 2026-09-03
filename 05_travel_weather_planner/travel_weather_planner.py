6b86bb00a97c1a79a212106544cb4bf648ae5f88
Yashwant Kumar <sahratan144@gmail.com>
2026-09-03 10:20:45 +0530

    first LAB

---
+++ 05_travel_weather_planner/travel_weather_planner.py
@@ -0,0 +1,27 @@
+distance_mi = 5
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print(False)
+elif distance_mi<=1:
+    if not is_raining:
+        print(True)
+    else:
+        print(False)
+
+elif distance_mi <=6:
+    if not is_raining and has_bike:
+        print(True)
+    else:
+        print(False)
+elif distance_mi >6:
+    if has_ride_share_app or has_car:
+        print(True)
+    else:
+        print(False)
+    
+
+
