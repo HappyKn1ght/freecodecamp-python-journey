b305434127051e10923cde37e1c00d0f4456ed6d
Yashwant Kumar <sahratan144@gmail.com>
2026-09-07 13:37:27 +0530

    sets

--- Dictionaries and Sets/sets.py
+++ Dictionaries and Sets/sets.py
@@ -1,3 +1,8 @@
 my_set = {1, 2, 3, 4, 5}
-my_set.add(6)
+my_set2 = {6, 7, 8, 9, 10}
+#my_set.add(6)
+my_set.update(my_set2)
+#my_set.discard(7) #don't throw error
+#my_set.remove(7) # throws KeyError if the element is not found
+#my_set.clear()
 print(my_set)
