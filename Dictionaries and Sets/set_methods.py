b529e703871efb853a1707b4e22999fdd565d9d8
Yashwant Kumar <sahratan144@gmail.com>
2026-09-07 13:45:54 +0530

    set methods performing

--- Dictionaries and Sets/set_methods.py
+++ Dictionaries and Sets/set_methods.py
@@ -1,10 +1,10 @@
 my_set = {1, 2, 3, 4, 5}
 your_set = {2, 3, 4, 6}
 
-print(your_set.issubset(my_set)) 
-print(my_set.issuperset(your_set)) 
-print(my_set.isdisjoint(your_set))
-print(my_set | your_set)
-print(my_set & your_set)
-print(my_set - your_set)
-print(my_set ^ your_set)
+print(f"issubset: {your_set.issubset(my_set)}")
+print(f"issuperset: {my_set.issuperset(your_set)}")
+print(f"isdisjoint: {my_set.isdisjoint(your_set)}")
+print(f"union: {my_set | your_set}")
+print(f"intersection: {my_set & your_set}")
+print(f"difference: {my_set - your_set}")
+print(f"symmetric_difference: {my_set ^ your_set}")
