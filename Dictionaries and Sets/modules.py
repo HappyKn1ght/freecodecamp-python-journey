ee87bf5a12f8fdcef0523455d195cb622b312dbd
Yashwant Kumar <sahratan144@gmail.com>
2026-09-07 14:22:15 +0530

    modules practice

---
+++ Dictionaries and Sets/modules.py
@@ -0,0 +1,18 @@
+# import math 
+# # import math as m
+# # print(m.sqrt(36))
+
+# print(f"square root of 6: {math.sqrt(36)}")
+
+from math import radians, sin, cos
+
+angle_degrees = 40
+angle_radians = radians(angle_degrees)
+
+sine_value = sin(angle_radians)
+cos_value = cos(angle_radians)
+
+# print(f"sin value: {sine_value:.4f}") 
+# print(f"cos value: {cos_value:.4f}") 
+print(f"sin value: {sine_value}") 
+print(f"cos value: {cos_value}") 
