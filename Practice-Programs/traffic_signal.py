cbceaa381166c4c39aa5e7daf6938ed3f7c9b59d
Yashwant Kumar <sahratan144@gmail.com>
2026-09-13 11:10:50 +0530

    practice-programs

---
+++ Practice-Programs/traffic_signal.py
@@ -0,0 +1,10 @@
+light = input("Enter Traffic Signal light:").lower()
+
+if(light=="red"):
+    print("Stop (x_x)")
+elif(light =="yellow"):
+    print("Look (O.O)")
+elif(light=="green"):
+    print("Go (=^.^=)")
+else:
+    print("Broken signal or invalid input!")
