bf9de32db51dc238d0e9dd038f5effbb72bc08ff
Yashwant Kumar <sahratan144@gmail.com>
2026-09-15 20:28:12 +0530

    simple interest

---
+++ Practice-Programs/simple_interest.py
@@ -0,0 +1,7 @@
+p = float(input("Enter Principal amount (p): "))
+r = float(input("Enter Rate of interest (r): "))
+t = float(input("Enter Time in years (t): "))
+
+si = (p*r*t/100)
+
+print(f"Simple Interest: {si}")
